2973b381db5028c4310418493dd6162ee1a3f783
Marquant Lucas <lucas.marquant2@etu.univ-lorraine.fr>
2018-03-10 12:07:41 +0100

    ajout des interceptors , de API, du store, de vuex et d autre truc

--- Application_Admin/admin/src/router/index.js
+++ Application_Admin/admin/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import signin from '@/components/signin'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'signin',
+      component: signin
     }
   ]
 })
